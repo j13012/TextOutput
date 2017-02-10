5ef4dd6b34c44bdfc965b2b256ca3423f6051ead
j13012 <j13012@sangi.jp>
2017-02-10 13:36:34 +0900

    commit TextOutput
    Completed

--- TextOutputTest/ViewController.swift
+++ TextOutputTest/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var btn: UIButton!
+    var value = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        btn.addTarget(self, action: #selector(buttonTapped(sender: )), for: .touchDown)
+        
+        self.view.addSubview(textField)
+        self.view.addSubview(btn)
+        self.view.addSubview(label)
+    }
+    
+    @IBAction func buttonTapped(sender: UIButton){
+        value = textField.text!
+        
+        label.text = value
     }
 
     override func didReceiveMemoryWarning() {
